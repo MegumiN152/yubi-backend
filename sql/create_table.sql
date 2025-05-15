cc5e13b731aca0d1ef5c378e4997e6b1f2062892
MegumiN152 <104641621+MegumiN152@users.noreply.github.com>
2025-05-15 08:20:17 +0800

    更新

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -37,3 +37,58 @@ create table if not exists chart
     updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete     tinyint      default 0                 not null comment '是否删除'
 ) comment '图表信息表' collate = utf8mb4_unicode_ci;
+
+create table if not exists team
+(
+    id          bigint auto_increment comment 'id'
+        primary key,
+    name        varchar(256)                       null comment '队伍名称',
+    imgUrl      varchar(512)                       null comment '队伍图片',
+    userId      bigint                             null comment '队长id',
+    description varchar(128)                       null comment '队伍描述',
+    maxNum      int                                null comment '最大人数',
+    createTime  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete    tinyint  default 0                 not null comment '是否删除'
+)
+    comment '队伍';
+
+create index idx_team_name
+    on yubi.team (name(10));
+
+create index idx_team_userId
+    on yubi.team (userId);
+
+create table if not exists team_chart
+(
+    id         bigint auto_increment comment 'id'
+        primary key,
+    teamId     bigint                             null comment '队伍id',
+    chartId    bigint                             null comment '图表id',
+    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
+)
+    comment '队伍图表关系表';
+
+create index idx_team_chart_chartId
+    on yubi.team_chart (chartId);
+
+create index idx_team_chart_teamId
+    on yubi.team_chart (teamId);
+
+create table if not exists team_user
+(
+    id         bigint auto_increment comment 'id'
+        primary key,
+    teamId     bigint                             null comment '队伍id',
+    userId     bigint                             null comment '用户id',
+    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
+)
+    comment '队伍用户关系表';
+
+create index idx_team_user_teamId
+    on yubi.team_user (teamId);
+
+create index idx_team_user_userId
+    on yubi.team_user (userId);
